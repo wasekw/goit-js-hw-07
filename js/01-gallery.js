1f3485fd6a7de0976c619f24a136c44634059317
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-10-24 23:22:11 +0300

    Ended 7 homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// // Change code below this line
+const placeForGallery = document.querySelector('.gallery');
+const markupGallery = makePictureForGallery(galleryItems);
+placeForGallery.insertAdjacentHTML('beforeend', markupGallery);
 
-console.log(galleryItems);
+function makePictureForGallery (picture) {
+  return picture.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join(''); 
+};
+
+placeForGallery.addEventListener('click', showPicture);
+
+function showPicture(event) {
+  event.preventDefault();
+  const isPictureChosen = event.target.classList.contains('gallery__image');
+  if (!isPictureChosen) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`, {
+    onClose: () => {
+      window.removeEventListener('keyup', closeModal);
+    },
+  });
+
+  instance.show();
+
+  document.addEventListener('keyup', closeModal);
+
+  function closeModal(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
+
